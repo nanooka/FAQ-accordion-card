c62df136b7869ee7a69427983d6c4f1d650d8f6c
nanooka <102428722+nanooka@users.noreply.github.com>
2023-04-05 21:30:25 +0400

    first commit

--- script.js
+++ script.js
@@ -2,9 +2,14 @@ const arrows = document.querySelectorAll(".q-a");
 const answers = document.querySelectorAll(".answer");
 
 arrows.forEach(function (arrow) {
-  arrow.addEventListener("click", (event) => {
-    arrow.classList.toggle("visible");
-    arrow.classList.toggle("bold");
-    arrow.classList.toggle("rotate");
+  arrow.addEventListener("click", () => {
+    arrows.forEach(function (otherArrow) {
+      otherArrow.classList.remove("visible");
+      otherArrow.classList.remove("bold");
+      otherArrow.classList.remove("rotate");
+    });
+    arrow.classList.add("visible");
+    arrow.classList.add("bold");
+    arrow.classList.add("rotate");
   });
 });
